483e72a426370f8ca426ac887095d604f71d8b5f
DESKTOP-8AQ5VUP\U3 <abdelrahman.yousry000@gmail.com>
2022-05-17 12:34:48 +0200

    Autosar like drivers

---
+++ Gpt_cfg.c
@@ -0,0 +1,6 @@
+/*
+ * Gpt_cfg.c
+ *
+ *  Created on: Apr 23, 2022
+ *      Author: Abdelrahman Yousry
+ */
---
+++ Gpt_cfg.h
@@ -0,0 +1,8 @@
+/*
+ * Gpt_cfg.h
+ *
+ *  Created on: Apr 23, 2022
+ *      Author: Abdelrahman Yousry
+ */
+
+#endif /* GPT_CFG_H_ */
